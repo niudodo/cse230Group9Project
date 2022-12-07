848f1bde781773d53b4ab9664a1078f2f94ec037
MartinNose <liucmartin@qq.com>
2022-12-06 21:46:36 -0800

    add norm

--- breakout/src/Geometry.hs
+++ breakout/src/Geometry.hs
@@ -9,3 +9,8 @@ Vector2 x1 y1 ^-^ Vector2 x2 y2 = Vector2 (x1 - x2) (y1 - y2)
 
 (^*^) :: Num s => s -> Vector2 s -> Vector2 s
 t ^*^ (Vector2 x1 x2) = Vector2 (t * x1) (t * x2)
+
+norm :: (Floating s, Eq s) => Vector2 s -> Vector2 s
+norm (Vector2 0.0 0.0) = (Vector2 0.0 0.0)
+norm (Vector2 x1 x2) = Vector2 (x1 / mod) (x2 / mod)
+                where mod = sqrt (x1 * x1 + x2 * x2)
